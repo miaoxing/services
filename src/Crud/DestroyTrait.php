37552691f2f43d5d2102241f33add6f5141ab430
twinh <twinhuang@qq.com>
2020-02-27 09:08:42 +0800

    refactor: crud 作为通用功能，移入 services 中

---
+++ src/Crud/DestroyTrait.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Miaoxing\Services\Crud;
+
+use Miaoxing\Plugin\BaseModelV2;
+use Miaoxing\Services\Service\Convention;
+use Miaoxing\Services\Service\Request;
+use Wei\RetTrait;
+
+/**
+ * @property Convention $convention
+ */
+trait DestroyTrait
+{
+    use RetTrait;
+
+    public function destroyAction($req)
+    {
+        $model = $this->convention->createModel($this)->findOneById($req['id']);
+
+        $ret = $this->beforeDestroy($req, $model);
+        if ($ret['code'] !== 1) {
+            return $ret;
+        }
+
+        $model->destroy();
+
+        return $this->suc();
+    }
+
+    protected function beforeDestroy(Request $req, BaseModelV2 $model)
+    {
+        return $this->suc();
+    }
+}
---
+++ src/Crud/FormTrait.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Miaoxing\Services\Crud;
+
+trait FormTrait
+{
+    use NewCreateTrait;
+    use EditUpdateTrait;
+}
