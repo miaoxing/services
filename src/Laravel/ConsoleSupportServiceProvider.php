b7ece91b477ba98bc7afecad9abc31ddf6e56a4c
twinh <twinhuang@qq.com>
2020-03-13 12:25:02 +0800

    refactor: 不加载未用到的命令

--- src/Laravel/ConsoleSupportServiceProvider.php
+++ src/Laravel/ConsoleSupportServiceProvider.php
@@ -2,14 +2,13 @@
 
 namespace Miaoxing\Services\Laravel;
 
-use Illuminate\Foundation\Providers\ArtisanServiceProvider;
 use Illuminate\Foundation\Providers\ComposerServiceProvider;
 
 class ConsoleSupportServiceProvider extends \Illuminate\Foundation\Providers\ConsoleSupportServiceProvider
 {
     protected $providers = [
         ArtisanServiceProvider::class,
-        //MigrationServiceProvider::class, 已有自定义的 migrate 命令，不加载 laravel 的命令
+//        MigrationServiceProvider::class, 已有自定义的 migrate 命令，不加载 laravel 的命令
         ComposerServiceProvider::class,
     ];
 }
