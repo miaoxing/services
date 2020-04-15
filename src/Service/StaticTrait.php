ad0f2b7c0e286876e6dc162ceb15dc77f406a60c
twinh <twinhuang@qq.com>
2020-04-15 20:27:07 +0800

    chore: 将静态调用逻辑加入服务容器中

--- src/Service/StaticTrait.php
+++ src/Service/StaticTrait.php
@@ -2,61 +2,24 @@
 
 namespace Miaoxing\Services\Service;
 
+use ReflectionException;
+
+/**
+ * @mixin \WeiMixin
+ */
 trait StaticTrait
 {
     /**
-     * Whether to check if the service method exists
-     *
-     * @var bool
-     */
-    protected static $checkServiceMethod = false;
-
-    /**
-     * @param string $method
-     * @return bool
-     * @throws \ReflectionException
-     * @noinspection UselessReturnInspection
-     */
-    protected static function isServiceMethod(string $method): bool
-    {
-        static $cache = [];
-        $exists = &$cache[static::class][$method];
-
-        if (isset($exists)) {
-            return $exists;
-        }
-
-        if (!method_exists(static::class, $method)) {
-            return $exists = false;
-        }
-
-        $ref = new \ReflectionMethod(static::class, $method);
-        if (!$ref->isProtected()) {
-            return $exists = false;
-        }
-
-        return $exists = strpos($ref->getDocComment(), "* @svc\n") !== false;
-    }
-
-    /**
-     * Call a service api method
+     * Call a service method
      *
      * @param string $method
      * @param array $args
      * @return mixed
-     * @throws \ReflectionException
+     * @throws ReflectionException
      */
     public function __call($method, $args)
     {
-        if (static::$checkServiceMethod) {
-            if (static::isServiceMethod($method)) {
-                return $this->$method(...$args);
-            }
-        } elseif (method_exists($this, $method)) {
-            return $this->$method(...$args);
-        }
-
-        return call_user_func_array($this->$method, $args);
+        return $this->wei->caller($this, $method)(...$args);
     }
 
     /**
@@ -65,41 +28,10 @@ trait StaticTrait
      * @param string $method
      * @param array $args
      * @return mixed
-     * @throws \ReflectionException
+     * @throws ReflectionException
      */
     public static function __callStatic(string $method, array $args)
     {
-        if (static::$checkServiceMethod && !static::isServiceMethod($method)) {
-            throw new \BadMethodCallException(sprintf('Service method "%s" not found', $method));
-        }
-
-        if (isset(static::$createNewInstance) && static::$createNewInstance) {
-            $instance = static::newInstance();
-        } else {
-            $instance = wei()->get(static::getServiceName());
-        }
-        return $instance->$method(...$args);
-    }
-
-    /**
-     * Initialize a new instance of current service
-     *
-     * @param array $options
-     * @return static
-     */
-    public static function newInstance($options = [])
-    {
-        return new static($options);
-    }
-
-    /**
-     * Receive the base name of current class
-     *
-     * @return string
-     */
-    protected static function getServiceName()
-    {
-        $parts = explode('\\', static::class);
-        return lcfirst(end($parts));
+        return Wei::staticCaller(static::class, $method)(...$args);
     }
 }
