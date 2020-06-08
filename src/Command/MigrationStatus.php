2d4f621fc85d46ff5f712a10649456362cae5320
twinh <twinhuang@qq.com>
2020-06-08 22:40:13 +0800

    chore: 更新说明

--- src/Command/MigrationStatus.php
+++ src/Command/MigrationStatus.php
@@ -7,19 +7,10 @@ use Miaoxing\Services\Service\Migration;
 
 class MigrationStatus extends BaseCommand
 {
-    /**
-     * The console command signature.
-     *
-     * @var string
-     */
-    protected $signature = 'mig:status';
-
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Output the migration status table';
+    protected function configure()
+    {
+        $this->setDescription('Output the migration status table');
+    }
 
     /**
      * Execute the console command.
