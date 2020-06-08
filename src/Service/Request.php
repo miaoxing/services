0432c019127a82a50649e4bdee8031b84eb60948
twinh <twinhuang@qq.com>
2020-06-08 17:05:37 +0800

    chore: 移除已更新到 wei 的方法

--- src/Service/Request.php
+++ src/Service/Request.php
@@ -4,15 +4,6 @@ namespace Miaoxing\Services\Service;
 
 class Request extends \Wei\Request
 {
-    public function __construct(array $options = array())
-    {
-        parent::__construct($options);
-
-        if (false !== strpos($this->getServer('HTTP_CONTENT_TYPE'), 'application/json')) {
-            $this->data += (array) json_decode($this->getContent(), true);
-        }
-    }
-
     public function json()
     {
         return $this->acceptJson();
