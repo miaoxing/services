2f6247ecfab3931e845dc337e715d87d3af73daf
twinh <twinhuang@qq.com>
2020-12-11 18:51:40 +0800

    refactor(Model)!: `data` 属性改为 `attributes`，相关属性和方法更新
    1. `data` 属性改为 `attributes`
    2. `getData` 方法改为 `getAttributes`
    3. `setData` 方法改为 `setAttributes`
    4. `changedData` 属性改为 `changes`
    5. `getChangedData` 方法改为 `getChanges`
    6. `loadData` 方法改为 `loadAttributes`
    7. `relationData` 属性改为 `relationAttributes`
    8. `setRelationData` 方法改为 `setRelationAttribute`
    9. `virtualData` 属性改为 `virtualAttributes`
    10. `setRawData` 方法改为 `setDbAttributes`
    11. `generateDbData` 方法改为 `getDbAttributes`
    
    BREAKING CHANGE: `data` 属性改为 `attributes`，相关属性和方法更新

--- src/Service/Db.php
+++ src/Service/Db.php
@@ -2,6 +2,8 @@
 
 namespace Miaoxing\Services\Service;
 
+use Miaoxing\Plugin\Service\WeiBaseModel;
+
 /**
  * @property \Wei\BaseCache $cache
  */
@@ -24,5 +26,25 @@ class Db extends \Wei\Db
                 }
             );
         }
+    }
+
+    /**
+     * Init a model instance
+     *
+     * @param string $table The name of database table
+     * @param array $attribuets The data for table record
+     * @param bool $isNew Whether it's a new record and have not save to database
+     * @return WeiBaseModel
+     */
+    public function init($table, $attribuets = [], $isNew = true)
+    {
+        $class = $this->getRecordClass($table);
+        return new $class([
+            'wei' => $this->wei,
+            'db' => $this,
+            'table' => $table,
+            'isNew' => $isNew,
+            'attributes' => $attribuets,
+        ]);
     }
 }
