4ea0744cc0fc411371303c3ab717627936605521
twinh <twinhuang@qq.com>
2023-07-05 23:45:44 +0800

    feat(services): 允许通过 `schedule` 缓存查看计划任务运行信息

--- src/ServicesPlugin.php
+++ src/ServicesPlugin.php
@@ -3,7 +3,11 @@
 namespace Miaoxing\Services;
 
 use Miaoxing\Plugin\BasePlugin;
+use Wei\Time;
 
+/**
+ * @mixin \CachePropMixin
+ */
 class ServicesPlugin extends BasePlugin
 {
     protected $name = '通用服务';
@@ -11,4 +15,11 @@ class ServicesPlugin extends BasePlugin
     protected $description = '存放一些通用的功能，连接底层类库和上层业务';
 
     protected $code = 201;
+
+    public function onSchedule()
+    {
+        $this->cache->set('schedule', [
+            'lastRunAt' => Time::now(),
+        ]);
+    }
 }
