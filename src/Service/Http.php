4b90f227d56831d90c966890a127288529a87d8a
twinh <twinhuang@qq.com>
2023-08-23 17:02:27 +0800

    feat(deprecated): 废弃 `Miaoxing\Services\Service\Http`，改用 `Wei\Http`

--- src/Service/Http.php
+++ src/Service/Http.php
@@ -2,28 +2,9 @@
 
 namespace Miaoxing\Services\Service;
 
-use Wei\Ret;
-use Wei\RetTrait;
-
+/**
+ * @deprecated Use \Wei\Http instead
+ */
 class Http extends \Wei\Http
 {
-    use RetTrait;
-
-    /**
-     * 返回 ret 格式的 HTTP 请求结果
-     *
-     * @param array $data
-     * @return Ret
-     */
-    public function toRet(array $data = [])
-    {
-        if ($this->isSuccess()) {
-            $response = $this->getResponse();
-            $extra = is_array($response) ? $response : ['data' => $response];
-            return $this->suc($data + $extra);
-        }
-
-        $e = $this->getErrorException();
-        return $this->err($data + ['code' => $e->getCode(), 'message' => $e->getMessage()]);
-    }
 }
