33459c75035dc9f3cb38ea4aa03c5cb16390b3b3
twinh <twinhuang@qq.com>
2023-12-31 10:58:27 +0800

    style: php-cs-fixer fully_qualified_strict_types

--- src/Laravel/ConsoleKernel.php
+++ src/Laravel/ConsoleKernel.php
@@ -21,7 +21,7 @@ class ConsoleKernel extends Kernel
     /**
      * Define the application's command schedule.
      *
-     * @param \Illuminate\Console\Scheduling\Schedule $schedule
+     * @param Schedule $schedule
      * @return void
      */
     protected function schedule(Schedule $schedule)
